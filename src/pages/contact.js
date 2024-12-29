bf73cae2bb83e217862ae20febf143d9dd805ab5
Ayabonga Qwabi <aya@simply.co.za>
2024-12-29 13:06:27 +0200

    fix missing components

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,5 @@
 import Head from 'next/head';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Footer from '@/components/Footer';
 import { NextSeo } from 'next-seo';
 
 export default function Contact() {
@@ -18,7 +17,6 @@ export default function Contact() {
         <title>Create Next App</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Header />
       <section class='text-gray-600 body-font relative'>
         <div class='container px-5 pt-24 pb-16 mx-auto'>
           <div class='flex flex-col text-center w-full mb-12'>
